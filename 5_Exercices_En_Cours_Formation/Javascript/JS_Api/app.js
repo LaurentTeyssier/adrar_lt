83ebd9f7b5d7eb778a7a80fc9f5c92caf32e1fa2
Laurent Teyssier <lau.tey@outlook.fr>
2023-07-27 17:02:31 +0200

    api modified

--- 5_Exercices_En_Cours_Formation/Javascript/JS_Api/app.js
+++ 5_Exercices_En_Cours_Formation/Javascript/JS_Api/app.js
@@ -11,3 +11,27 @@ async function contactAPI(){
 }
 
 contactAPI();
+
+const pokemonListe = document.getElementById('pokeListe');
+console.log(pokemonListe);
+// de base une ƒ° => est anonyme, astuce pour désanonymiser, on la stocke dans une variable
+const pokemonApiContact = async () => {
+    //Data va récup Toutes les données de l'api
+    const pokemonData = await fetch('https://pokeapi.co/api/v2/pokemon');
+    console.log('pokemonData',pokemonData);
+    //Plutot que de Travailler sur la réponse, on va la transformé pour 
+    //qu'elle deviennt un OBJET JS (+ pratique)
+    const pokemonDataTransformed = await pokemonData.json();
+    console.log('pokemonDataTransformed',pokemonDataTransformed);
+    console.log(pokemonDataTransformed.results[0].name);
+    // Boucle pour parcourir le tableau results dans la réponse
+    for(let i=0;i<pokemonDataTransformed.results.length;i++){
+        let listElement = document.createElement('p');
+        listElement.innerText = pokemonDataTransformed.results[i].name;
+        pokemonListe.append(listElement);
+    }
+    const pokemonName = document.createElement('h1');
+    pokemonName.innerText = pokemonDataTransformed.results[0].name;
+    document.body.append(pokemonName);
+};
+pokemonApiContact();
